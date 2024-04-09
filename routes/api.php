03d8c9cfb7cb199dd184899c47ad9a6ec1160d34
Mirza Haseeb Baig <mirza.baig97@hotmail.com>
2024-04-09 16:53:32 +0500

    testing api to call Message Event

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Events\MessageSent;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ChatsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::get('test', function () {
+    event(new MessageSent());
 });
