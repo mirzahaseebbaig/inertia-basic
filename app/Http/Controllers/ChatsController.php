4cf120f5dc96f654c92a58973c5df0a40e2d310e
Mirza Haseeb Baig <mirza.baig97@hotmail.com>
2024-05-09 00:41:51 +0500

    Basic chat controller added.

--- app/Http/Controllers/ChatsController.php
+++ app/Http/Controllers/ChatsController.php
@@ -2,12 +2,34 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSent;
+use App\Models\Message;
+use Illuminate\Http\Request;
+use Illuminate\Mail\Events\MessageSending;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class ChatsController extends Controller
 {
+    //Add the below functions
+
+
     public function index()
     {
         return Inertia::render('chat');
+    }
+
+    public function fetchMessages()
+    {
+        return Message::with('user')->get();
+    }
+
+    public function sendMessage(Request $request)
+    {
+        $user = Auth::user();
+        $message = $user->messages()->create([
+            'message' => $request->input('message')
+        ]);
+        return ['status' => 'Message Sent!'];
     }
 }
