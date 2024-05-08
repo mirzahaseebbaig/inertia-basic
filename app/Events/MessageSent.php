c805fc23aab34c619d77a7c4752cdf6b9aa6523a
Mirza Haseeb Baig <mirza.baig97@hotmail.com>
2024-05-09 00:41:39 +0500

    Message related Event added for pusher.

--- app/Events/MessageSent.php
+++ app/Events/MessageSent.php
@@ -3,6 +3,7 @@
 namespace App\Events;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
@@ -10,22 +11,30 @@ use Illuminate\Queue\SerializesModels;
 class MessageSent implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
+    protected int $userId;
 
     /**
      * Create a new event instance.
      */
-    /*public function __construct()
+    public function __construct($userId)
     {
-        //
-    }*/
+        $this->userId = $userId;
+    }
 
     public function broadcastOn()
     {
-        return ['chat'];
+        return new PrivateChannel('chat.'.$this->userId);
     }
 
     public function broadcastAs()
     {
         return 'chat.notification';
+    }
+
+    public function broadcastWith()
+    {
+        return [
+            'user_id' => $this->userId,
+        ];
     }
 }
